27743bc8e39050a1c1252ab89c61c55bf9fe3629
rudxde <kj4derechte@gmail.com>
2021-05-03 10:06:58 +0200

    test: add unit tests for normalize path

---
+++ packages/lerna-dockerize/test/normalize-path.spec.ts
@@ -0,0 +1,31 @@
+import 'jasmine';
+import { normalizePath } from '../src/normalize-path';
+
+describe('normalizePath', () => {
+    it('should not change empty strings', () => {
+        const input = '';
+        expect(normalizePath(input)).toEqual(input);
+    });
+    it('should not change filenames without path slashes', () => {
+        const input = 'document.pdf';
+        expect(normalizePath(input)).toEqual(input);
+    });
+    it('should not change unix style paths', () => {
+        const input = '/tmp/test/foo.bar';
+        expect(normalizePath(input)).toEqual(input);
+    });
+    it('should replace backslashes to be unix like paths', () => {
+        const input = '/tmp/test/foo.bar';
+        expect(normalizePath(input)).toEqual(input);
+    });
+    it('should replace backslashes to be unix like paths', () => {
+        const input = '.\\files\\test\\1.pdf';
+        const expected = './files/test/1.pdf';
+        expect(normalizePath(input)).toEqual(expected);
+    });
+    it('should replace backslashes in mixed style paths to be unix like paths', () => {
+        const input = './files/test\\1.pdf';
+        const expected = './files/test/1.pdf';
+        expect(normalizePath(input)).toEqual(expected);
+    });
+});
