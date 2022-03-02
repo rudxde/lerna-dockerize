d4ec08f83604dd6e215591504e93f3956542924f
rudxDe <kj4derechte@gmail.com>
2022-03-02 01:14:42 +0100

    feat(init): add description to options

--- packages/init/src/index.ts
+++ packages/init/src/index.ts
@@ -7,27 +7,33 @@ export const command: CommandModule<{}, IInitArgs> = {
     describe: 'Initialize lerna-dockerize in your project',
     builder: {
         workingDirectory: {
+            description: 'The working directory to run the command in',
             type: 'string',
             defaultDescription: 'current working directory',
         },
         installAsDevDependency: {
+            description: 'Install as devDependency',
             type: 'boolean',
             default: true,
         },
         packageManager: {
+            description: 'The package manager to use',
             type: 'string',
             default: 'npm',
             choices: ['npm', 'yarn'],
         },
         templateDockerFileName: {
+            description: 'The name for the template Dockerfile',
             type: 'string',
             default: 'Dockerfile.template',
         },
         baseDockerfileName: {
+            description: 'The name for the base Dockerfile',
             type: 'string',
             default: 'Dockerfile.base',
         },
         scriptName: {
+            description: 'The name for the script added to package.json',
             type: 'string',
             default: 'lerna-dockerize',
         },
